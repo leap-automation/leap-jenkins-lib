e626f9fdb29fdc904e4095dc64465e959929fa80
karuna <karunakaran.ks@cognizant.com>
2020-06-29 16:43:16 +0530

    added regression bot support

--- vars/getRecommendedTests.groovy
+++ vars/getRecommendedTests.groovy
@@ -27,9 +27,9 @@ def call(projectId, modelId,
     if (tests == null || !isCollectionOrArray(tests)) {
         error "unable get regression tests" + parseError(tests)
     }
-    if (tests == []) {
-        error "no regression tests returned"
-    }
+//    if (tests == []) {
+//        error "no regression tests returned"
+//    }
     if (pattern != null) {
         tests = tests.collect { replacePatternWithMap(it) }.join(sep)
         debug tests
